f5b14ec825b30211e2a58e3a6a503a26ae090fd3
Jerry9634 <jerry.son77@gmail.com>
2023-09-23 02:16:32 +0900

    viewer highlights labeled values

--- run.bat
+++ run.bat
@@ -1,14 +1,20 @@
 cd .\car-plug-server
 start "" npm start
 
+timeout /t 3
+
 cd ..\car-plug-viewer
 start "" serve -s -l 3001
 
 cd ..\dashboard-pc\dashboard-proxy-server
 start "" npm start
 
+timeout /t 1
+
 cd ..\dashboard-client
 start "" serve -s -l 3002
+
+timeout /t 1
 
 start chrome http://localhost:3001/
 start chrome http://localhost:3002/
