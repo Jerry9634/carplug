51517d0a620b3490a6dd44bb075fe93f9a1e2bba
Jerry Son <jerry.son77@gmail.com>
2023-11-24 16:53:17 +0900

    vehicle simulator and touchscreen simulator integrated

--- run.bat
+++ run.bat
@@ -3,17 +3,14 @@ start "" car-plug-server-win
 
 timeout /t 3
 
-cd ..\vehicle-simulator
+cd ..\vehicle-simulator\proxy-server
+start "" touchscreen-proxy-server-win
+
+cd ..
 start "" serve -s -l 3000
 
 cd ..\car-plug-viewer
 start "" serve -s -l 3001
-
-cd ..\touchscreen-pc\touchscreen-proxy-server
-start "" touchscreen-proxy-server-win
-
-cd ..\touchscreen-client
-start "" serve -s -l 3002
 
 timeout /t 3
 
